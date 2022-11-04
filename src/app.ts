dd1f3933fd8b633e695ff852aa2d7619f689fcce
ProditorMagnus <public.ravana@gmail.com>
2022-11-04 12:01:38 +0200

    SalVaTion siege alert role

--- src/app.ts
+++ src/app.ts
@@ -86,7 +86,7 @@ const config = {
       serverOffset: 3,
       enabled: true,
       advanceWarningTime: 5 * Utils.minuteMs,
-      pingRole: "0",
+      pingRole: "1032659524877832192",
       pingMessage: "Time to siege!",
       outputChannel: "1038023007475605554", // SalVaTion
       cleanConfig: {
